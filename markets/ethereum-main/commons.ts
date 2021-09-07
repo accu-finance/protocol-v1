9949e7d4f52be79ccbe931fb599a209e03301e41
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2021-09-07 16:28:31 +0700

    Create markets

---
+++ markets/ethereum-main/commons.ts
@@ -0,0 +1,21 @@
+import {parseUnits} from 'ethers/lib/utils';
+import {PERCENTAGE_DECIMALS} from '../../constants';
+import {BaseConfiguration, NativeCurrency, Oracle} from '../../types';
+
+export const baseConfig: BaseConfiguration = {
+  marketId: 'Base Market',
+  nativeCurrency: NativeCurrency.ETH,
+  aTokenNamePrefix: 'Accu interest bearing',
+  stableDebtTokenNamePrefix: 'Accu stable debt bearing',
+  variableDebtTokenNamePrefix: 'Accu variable debt bearing',
+  symbolPrefix: '',
+  providerId: 0, // Overriden in index.ts
+  protocolGlobalConfig: {
+    TokenDistributorPercentageBase: parseUnits('100', PERCENTAGE_DECIMALS),
+    priceUSDAddress: '0x10F7Fc1F91Ba351f9C629c5947AD69bD03C05b96',
+    Referral: '0',
+  },
+  mockUsdPrice: '5848466240000000',
+  interestRateStrategies: [],
+  oracle: Oracle.Band,
+};
---
+++ markets/ethereum-main/rateStrategies.ts
@@ -0,0 +1,103 @@
+import {constants, utils} from 'ethers';
+import {RAY_DECIMALS} from '../../constants';
+import {InterestRateStrategyConfig, InterestRateStrategyName} from '../../types';
+
+const {Zero} = constants;
+
+// BUSD SUSD
+export const rateStrategyStableOne: InterestRateStrategyConfig = {
+  name: InterestRateStrategyName.rateStrategyStableOne,
+  optimalUtilizationRate: utils.parseUnits('0.8', RAY_DECIMALS),
+  baseVariableBorrowRate: Zero,
+  variableRateSlope1: utils.parseUnits('0.04', RAY_DECIMALS),
+  variableRateSlope2: utils.parseUnits('1', RAY_DECIMALS),
+  stableRateSlope1: Zero,
+  stableRateSlope2: Zero,
+};
+
+// DAI TUSD
+export const rateStrategyStableTwo: InterestRateStrategyConfig = {
+  name: InterestRateStrategyName.rateStrategyStableTwo,
+  optimalUtilizationRate: utils.parseUnits('0.8', RAY_DECIMALS),
+  baseVariableBorrowRate: Zero,
+  variableRateSlope1: utils.parseUnits('0.04', RAY_DECIMALS),
+  variableRateSlope2: utils.parseUnits('0.75', RAY_DECIMALS),
+  stableRateSlope1: utils.parseUnits('0.02', RAY_DECIMALS),
+  stableRateSlope2: utils.parseUnits('0.75', RAY_DECIMALS),
+};
+
+// USDC USDT
+export const rateStrategyStableThree: InterestRateStrategyConfig = {
+  name: InterestRateStrategyName.rateStrategyStableThree,
+  optimalUtilizationRate: utils.parseUnits('0.9', RAY_DECIMALS),
+  baseVariableBorrowRate: Zero,
+  variableRateSlope1: utils.parseUnits('0.04', RAY_DECIMALS),
+  variableRateSlope2: utils.parseUnits('0.6', RAY_DECIMALS),
+  stableRateSlope1: utils.parseUnits('0.02', RAY_DECIMALS),
+  stableRateSlope2: utils.parseUnits('0.6', RAY_DECIMALS),
+};
+
+// WETH
+export const rateStrategyETH: InterestRateStrategyConfig = {
+  name: InterestRateStrategyName.rateStrategyETH,
+  optimalUtilizationRate: utils.parseUnits('0.65', RAY_DECIMALS),
+  baseVariableBorrowRate: Zero,
+  variableRateSlope1: utils.parseUnits('0.08', RAY_DECIMALS),
+  variableRateSlope2: utils.parseUnits('1', RAY_DECIMALS),
+  stableRateSlope1: utils.parseUnits('0.1', RAY_DECIMALS),
+  stableRateSlope2: utils.parseUnits('1', RAY_DECIMALS),
+};
+
+// AAVE
+export const rateStrategyAAVE: InterestRateStrategyConfig = {
+  name: InterestRateStrategyName.rateStrategyACCRUE,
+  optimalUtilizationRate: utils.parseUnits('0.45', RAY_DECIMALS),
+  baseVariableBorrowRate: Zero,
+  variableRateSlope1: Zero,
+  variableRateSlope2: Zero,
+  stableRateSlope1: Zero,
+  stableRateSlope2: Zero,
+};
+
+// BAT ENJ LINK MANA MKR REN YFI ZRX
+export const rateStrategyVolatileOne: InterestRateStrategyConfig = {
+  name: InterestRateStrategyName.rateStrategyVolatileOne,
+  optimalUtilizationRate: utils.parseUnits('0.45', RAY_DECIMALS),
+  baseVariableBorrowRate: Zero,
+  variableRateSlope1: utils.parseUnits('0.07', RAY_DECIMALS),
+  variableRateSlope2: utils.parseUnits('3', RAY_DECIMALS),
+  stableRateSlope1: utils.parseUnits('0.1', RAY_DECIMALS),
+  stableRateSlope2: utils.parseUnits('3', RAY_DECIMALS),
+};
+
+// KNC WBTC
+export const rateStrategyVolatileTwo: InterestRateStrategyConfig = {
+  name: InterestRateStrategyName.rateStrategyVolatileTwo,
+  optimalUtilizationRate: utils.parseUnits('0.65', RAY_DECIMALS),
+  baseVariableBorrowRate: Zero,
+  variableRateSlope1: utils.parseUnits('0.08', RAY_DECIMALS),
+  variableRateSlope2: utils.parseUnits('3', RAY_DECIMALS),
+  stableRateSlope1: utils.parseUnits('0.1', RAY_DECIMALS),
+  stableRateSlope2: utils.parseUnits('3', RAY_DECIMALS),
+};
+
+// SNX
+export const rateStrategyVolatileThree: InterestRateStrategyConfig = {
+  name: InterestRateStrategyName.rateStrategyVolatileThree,
+  optimalUtilizationRate: utils.parseUnits('0.65', RAY_DECIMALS),
+  baseVariableBorrowRate: Zero,
+  variableRateSlope1: utils.parseUnits('0.08', RAY_DECIMALS),
+  variableRateSlope2: utils.parseUnits('3', RAY_DECIMALS),
+  stableRateSlope1: utils.parseUnits('0.1', RAY_DECIMALS),
+  stableRateSlope2: utils.parseUnits('3', RAY_DECIMALS),
+};
+
+export const rateStrategyVolatileFour: InterestRateStrategyConfig = {
+  name: InterestRateStrategyName.rateStrategyVolatileFour,
+  optimalUtilizationRate: utils.parseUnits('0.45', RAY_DECIMALS),
+  baseVariableBorrowRate: Zero,
+  variableRateSlope1: utils.parseUnits('0.07', RAY_DECIMALS),
+  variableRateSlope2: utils.parseUnits('3', RAY_DECIMALS),
+  stableRateSlope1: Zero,
+  stableRateSlope2: Zero,
+};

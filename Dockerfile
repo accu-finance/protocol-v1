9a9702184033b288b1b0ee178494b03f98bd2a79
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2021-09-07 16:44:15 +0700

    Update package.json and helper files

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,24 @@
-FROM ethereum/solc:0.6.12 as build-deps
+FROM node:16.5.0-alpine3.14
 
-FROM node:14
-COPY --from=build-deps /usr/bin/solc /usr/bin/solc
+WORKDIR /app
+
+COPY package.json /app/package.json
+COPY package-lock.json /app/package-lock.json
+COPY hardhat.config.ts /app/hardhat.config.ts
+COPY tsconfig.json /app/tsconfig.json
+COPY .setup.js /app/.setup.js
+
+COPY contracts /app/contracts
+COPY constants /app/constants
+COPY markets /app/markets
+COPY deploy /app/deploy
+COPY types /app/types
+COPY utils /app/utils
+
+RUN npm i
+
+EXPOSE 8545
+
+ENTRYPOINT [ "npm" ]
+
+CMD [ "run", "hardhat" ]
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,140 +1,108 @@
-import path from 'path';
-import fs from 'fs';
-import { HardhatUserConfig } from 'hardhat/types';
-// @ts-ignore
-import { accounts } from './test-wallets.js';
-import { eEthereumNetwork, eNetwork, ePolygonNetwork, eXDaiNetwork } from './helpers/types';
-import { BUIDLEREVM_CHAINID, COVERAGE_CHAINID } from './helpers/buidler-constants';
-import {
-  NETWORKS_RPC_URL,
-  NETWORKS_DEFAULT_GAS,
-  BLOCK_TO_FORK,
-  buildForkConfig,
-} from './helper-hardhat-config';
-
-require('dotenv').config();
-
 import '@nomiclabs/hardhat-ethers';
-import '@nomiclabs/hardhat-waffle';
-import 'temp-hardhat-etherscan';
+import '@nomiclabs/hardhat-solhint';
+import '@tenderly/hardhat-tenderly';
+import 'dotenv/config';
+import 'hardhat-deploy';
 import 'hardhat-gas-reporter';
 import 'hardhat-typechain';
-import '@tenderly/hardhat-tenderly';
+import {HardhatUserConfig} from 'hardhat/config';
 import 'solidity-coverage';
-import { fork } from 'child_process';
+import getAccount from './utils/getAccounts';
+import getNodeUrl from './utils/getNodeUrl';
 
-const SKIP_LOAD = process.env.SKIP_LOAD === 'true';
-const DEFAULT_BLOCK_GAS_LIMIT = 12450000;
-const DEFAULT_GAS_MUL = 5;
-const HARDFORK = 'istanbul';
-const ETHERSCAN_KEY = process.env.ETHERSCAN_KEY || '';
-const MNEMONIC_PATH = "m/44'/60'/0'/0";
-const MNEMONIC = process.env.MNEMONIC || '';
-const UNLIMITED_BYTECODE_SIZE = process.env.UNLIMITED_BYTECODE_SIZE === 'true';
-
-// Prevent to load scripts before compilation and typechain
-if (!SKIP_LOAD) {
-  ['misc', 'migrations', 'dev', 'full', 'verifications', 'deployments', 'helpers'].forEach(
-    (folder) => {
-      const tasksPath = path.join(__dirname, 'tasks', folder);
-      fs.readdirSync(tasksPath)
-        .filter((pth) => pth.includes('.ts'))
-        .forEach((task) => {
-          require(`${tasksPath}/${task}`);
-        });
-    }
-  );
-}
-
-require(`${path.join(__dirname, 'tasks/misc')}/set-bre.ts`);
-
-const getCommonNetworkConfig = (networkName: eNetwork, networkId: number) => ({
-  url: NETWORKS_RPC_URL[networkName],
-  hardfork: HARDFORK,
-  blockGasLimit: DEFAULT_BLOCK_GAS_LIMIT,
-  gasMultiplier: DEFAULT_GAS_MUL,
-  gasPrice: NETWORKS_DEFAULT_GAS[networkName],
-  chainId: networkId,
-  accounts: {
-    mnemonic: MNEMONIC,
-    path: MNEMONIC_PATH,
-    initialIndex: 0,
-    count: 20,
-  },
-});
-
-let forkMode;
-
-const buidlerConfig: HardhatUserConfig = {
+const config: HardhatUserConfig = {
   solidity: {
-    version: '0.6.12',
-    settings: {
-      optimizer: { enabled: true, runs: 200 },
-      evmVersion: 'istanbul',
+    compilers: [
+      {
+        version: '0.6.12',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    ],
+  },
+  networks: {
+    hardhat: {
+      accounts: getAccount(),
+      // mining: {
+      //   auto: false,
+      //   interval: [3000, 6000],
+      // },
+    },
+    localhost: {
+      url: getNodeUrl('localhost'),
+      accounts: getAccount(),
+    },
+    mainnet: {
+      url: getNodeUrl('mainnet'),
+      accounts: getAccount('mainnet'),
+    },
+    rinkeby: {
+      url: getNodeUrl('rinkeby'),
+      accounts: getAccount('rinkeby'),
+    },
+    kovan: {
+      url: getNodeUrl('kovan'),
+      accounts: getAccount('kovan'),
+    },
+    bsctestnet: {
+      url: getNodeUrl('bsctestnet'),
+      accounts: getAccount('bsctestnet'),
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    admin: {
+      default: 0,
+    },
+    emergencyAdmin: {
+      default: 1,
+    },
+    providerRegistryOwner: {
+      default: 3,
+    },
+    user1: {
+      default: 4,
+    },
+    user2: {
+      default: 5,
+    },
+    user3: {
+      default: 6,
+    },
+    user4: {
+      default: 7,
+    },
+    user5: {
+      default: 8,
+    },
+    liquidator: {
+      default: 9,
+    },
+    treasury: {
+      default: 10,
     },
   },
   typechain: {
-    outDir: 'types',
+    outDir: 'typechain',
     target: 'ethers-v5',
-  },
-  etherscan: {
-    apiKey: ETHERSCAN_KEY,
-  },
-  mocha: {
-    timeout: 0,
   },
   tenderly: {
     project: process.env.TENDERLY_PROJECT || '',
     username: process.env.TENDERLY_USERNAME || '',
-    forkNetwork: '1', //Network id of the network we want to fork
   },
-  networks: {
-    coverage: {
-      url: 'http://localhost:8555',
-      chainId: COVERAGE_CHAINID,
-    },
-    kovan: getCommonNetworkConfig(eEthereumNetwork.kovan, 42),
-    ropsten: getCommonNetworkConfig(eEthereumNetwork.ropsten, 3),
-    main: getCommonNetworkConfig(eEthereumNetwork.main, 1),
-    tenderlyMain: getCommonNetworkConfig(eEthereumNetwork.tenderlyMain, 3030),
-    matic: getCommonNetworkConfig(ePolygonNetwork.matic, 137),
-    mumbai: getCommonNetworkConfig(ePolygonNetwork.mumbai, 80001),
-    xdai: getCommonNetworkConfig(eXDaiNetwork.xdai, 100),
-    hardhat: {
-      hardfork: 'berlin',
-      blockGasLimit: DEFAULT_BLOCK_GAS_LIMIT,
-      gas: DEFAULT_BLOCK_GAS_LIMIT,
-      gasPrice: 8000000000,
-      allowUnlimitedContractSize: UNLIMITED_BYTECODE_SIZE,
-      chainId: BUIDLEREVM_CHAINID,
-      throwOnTransactionFailures: true,
-      throwOnCallFailures: true,
-      accounts: accounts.map(({ secretKey, balance }: { secretKey: string; balance: string }) => ({
-        privateKey: secretKey,
-        balance,
-      })),
-      forking: buildForkConfig(),
-    },
-    buidlerevm_docker: {
-      hardfork: 'berlin',
-      blockGasLimit: 9500000,
-      gas: 9500000,
-      gasPrice: 8000000000,
-      chainId: BUIDLEREVM_CHAINID,
-      throwOnTransactionFailures: true,
-      throwOnCallFailures: true,
-      url: 'http://localhost:8545',
-    },
-    ganache: {
-      url: 'http://ganache:8545',
-      accounts: {
-        mnemonic: 'fox sight canyon orphan hotel grow hedgehog build bless august weather swarm',
-        path: "m/44'/60'/0'/0",
-        initialIndex: 0,
-        count: 20,
-      },
-    },
+  mocha: {
+    timeout: 600000,
+  },
+  gasReporter: {
+    enabled: false,
+    currency: 'USD',
   },
 };
 
-export default buidlerConfig;
+export default config;
